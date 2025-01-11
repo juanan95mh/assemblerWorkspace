48002acec67103c2267aee8374c8048d481993aa
juanan95mh <juananmh95@gmail.com>
2025-01-11 12:01:52 +0100

    Script compilacion y practicas

---
+++ procedures/procedures.s
@@ -0,0 +1,37 @@
+global  _start
+
+section .data
+    message db "Fibonacci Sequence:", 0x0a
+
+section .text
+_start:
+    call printMessage   ; print intro message
+    call initFib        ; set initial Fib values
+    call loopFib        ; calculate Fib numbers
+    call Exit           ; Exit the program
+
+printMessage:
+    mov rax, 1      ; rax: syscall number 1
+    mov rdi, 1      ; rdi: fd 1 for stdout
+    mov rsi,message ; rsi: pointer to message
+    mov rdx, 20     ; rdx: print length of 20 bytes
+    syscall         ; call write syscall to the intro message
+    ret
+
+initFib:
+    xor rax, rax    ; initialize rax to 0
+    xor rbx, rbx    ; initialize rbx to 0
+    inc rbx         ; increment rbx to 1
+    ret
+
+loopFib:
+    add rax, rbx    ; get the next number
+    xchg rax, rbx   ; swap values
+    cmp rbx, 10		; do rbx - 10
+    js loopFib		; jump if result is <0
+    ret
+
+Exit:
+    mov rax, 60
+    mov rdi, 0
+    syscall
